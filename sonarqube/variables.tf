929d8224b0737cdc9101e7a81ce4dac10f3b1341
Sam Hanot <sam.hanot@student.kuleuven.be>
2023-08-22 16:05:09 +0200

    fixed variables

--- sonarqube/variables.tf
+++ sonarqube/variables.tf
@@ -11,6 +11,26 @@ variable "private_data_cidrs" {
   type = list(any)
 }
 
+variable "app_lb_name" {}
+variable "app_lb_type" {}
+variable "app_lb_listener_port" {}
+variable "app_lb_target_port" {}
+variable "app_lb_protocol" {}
+
+variable "net_lb_name" {}
+variable "net_lb_type" {}
+variable "net_lb_listener_port" {}
+variable "net_lb_target_port" {}
+variable "net_lb_protocol" {}
+
+variable "app_instance_type" {}
+variable "app_instance_count" {}
+variable "app_file_path" {}
+
+variable "search_instance_type" {}
+variable "search_instance_count" {}
+variable "search_file_path" {}
+
 variable "postgres_username" {}
 variable "postgres_password" {}
 
